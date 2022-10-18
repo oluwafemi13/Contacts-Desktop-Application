fed4a954c08807ddbbf2645bb193c8d7cbca5eac
oluwafemi13 <sundayjabikem@gmail.com>
2022-10-18 23:14:46 +0100

    update on contact Model

---
+++ Models/Contact.cs
@@ -0,0 +1,40 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts_Desktop_Application.Models
+{
+    public class Contact
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [Ignore]
+        public string FullName
+        { 
+
+            get {
+
+                return FirstName + LastName;
+
+                }       
+         }
+
+        [MinLength(1)]
+        public string Phone { get; set; }
+
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+    }
+}
